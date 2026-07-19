33430419d118b5529f2f8f01e6acfe714542796d
23rd <23rd@vivaldi.net>
2026-07-19 11:28:47 +0300

    Added account selector to devices screen.

---
+++ TMessagesProj/src/main/java/org/telegram/messenger/NotificationsService.java
@@ -0,0 +1,78 @@
+/*
+ * This is the source code of Telegram for Android v. 1.3.x.
+ * It is licensed under GNU GPL v. 2 or later.
+ * You should have received a copy of the license in this archive (see LICENSE).
+ *
+ * Copyright Nikolai Kudashov, 2013-2018.
+ */
+
+package org.telegram.messenger;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Build;
+import android.os.IBinder;
+import androidx.core.app.NotificationCompat;
+import android.util.Log;
+
+public class NotificationsService extends Service {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            int pendingIntentFlags;
+            if (Build.VERSION.SDK_INT >= 34) {
+                pendingIntentFlags = PendingIntent.FLAG_IMMUTABLE;
+            } else {
+                pendingIntentFlags = PendingIntent.FLAG_MUTABLE;
+            }
+            String CHANNEL_ID = "push_service_channel";
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,"Push Notifications Service",NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+            Intent explainIntent = new Intent("android.intent.action.VIEW");
+            explainIntent.setData(Uri.parse("https://github.com/forkgram/TelegramAndroid"));
+            try {
+            PendingIntent explainPendingIntent = PendingIntent.getActivity(this, 0, explainIntent, pendingIntentFlags);
+            Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setContentIntent(explainPendingIntent)
+                    .setShowWhen(false)
+                    .setOngoing(true)
+                    .setSmallIcon(R.drawable.notification)
+                    .setContentText("Push service: tap to learn more").build();
+            startForeground(9999,notification);
+            } catch (Throwable ignore) {
+                Log.d("Fork Client", "Failed to set intent");
+            }
+        }
+        ApplicationLoader.postInitApplication();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        SharedPreferences preferences = MessagesController.getGlobalNotificationsSettings();
+        if (preferences.getBoolean("pushService", true)) {
+            Intent intent = new Intent("org.telegram.start");
+            intent.setPackage(getPackageName());
+            sendBroadcast(intent);
+        }
+    }
+}
